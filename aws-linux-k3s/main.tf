d3c086831d3435f73c38e6508e6a0c656d8faf6b
Cody Green <cody@codygreen.com>
2022-07-26 16:40:54 -0500

    k3s

---
+++ aws-linux-k3s/main.tf
@@ -0,0 +1,226 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "0.4.3"
+    }
+  }
+}
+
+# Last updated 2022-05-31
+# aws ec2 describe-regions | jq -r '[.Regions[].RegionName] | sort'
+variable "region" {
+  description = "What region should your workspace live in?"
+  default     = "us-east-1"
+  validation {
+    condition = contains([
+      "ap-northeast-1",
+      "ap-northeast-2",
+      "ap-northeast-3",
+      "ap-south-1",
+      "ap-southeast-1",
+      "ap-southeast-2",
+      "ca-central-1",
+      "eu-central-1",
+      "eu-north-1",
+      "eu-west-1",
+      "eu-west-2",
+      "eu-west-3",
+      "sa-east-1",
+      "us-east-1",
+      "us-east-2",
+      "us-west-1",
+      "us-west-2"
+    ], var.region)
+    error_message = "Invalid region!"
+  }
+}
+
+variable "instance_type" {
+  description = "What instance type should your workspace use?"
+  default     = "t3.micro"
+  validation {
+    condition = contains([
+      "t3.micro",
+      "t3.small",
+      "t3.medium",
+      "t3.large",
+      "t3.xlarge",
+      "t3.2xlarge",
+    ], var.instance_type)
+    error_message = "Invalid instance type!"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "coder_workspace" "me" {
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+resource "coder_agent" "main" {
+  arch = "amd64"
+  auth = "aws-instance-identity"
+  os   = "linux"
+  startup_script = <<EOF
+    #!/bin/sh
+    sudo snap install k9s
+    curl -sfL https://get.k3s.io | sh -
+    curl -fsSL https://code-server.dev/install.sh | sh
+    code-server --auth none --port 13337
+  EOF
+}
+
+resource "coder_app" "code-server" {
+  agent_id      = coder_agent.main.id
+  name          = "code-server"
+  icon          = "/icon/code.svg"
+  url           = "http://localhost:13337"
+  relative_path = true
+}
+
+locals {
+
+  # User data is used to stop/start AWS instances. See:
+  # https://github.com/hashicorp/terraform-provider-aws/issues/22
+
+  user_data_start = <<EOT
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+hostname: ${lower(data.coder_workspace.me.name)}
+users:
+- name: ${local.linux_user}
+  sudo: ALL=(ALL) NOPASSWD:ALL
+  shell: /bin/bash
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo -u ${local.linux_user} sh -c '${coder_agent.main.init_script}'
+
+--//--
+EOT
+
+  user_data_end = <<EOT
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+sudo shutdown -h now
+--//--
+EOT
+
+  # Ensure Coder username is a valid Linux username
+  linux_user = lower(substr(data.coder_workspace.me.owner, 0, 32))
+
+}
+
+#
+# Create the VPC
+#
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name                 = format("%s-vpc", lower(data.coder_workspace.me.name) )
+  cidr                 = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  #azs = var.availabilityZones
+
+  tags = {
+    Name        = format("%s-vpc", lower(data.coder_workspace.me.name))
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = module.vpc.vpc_id
+
+  tags = {
+    Name = "default"
+  }
+}
+resource "aws_route_table" "internet-gw" {
+  vpc_id = module.vpc.vpc_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = cidrsubnet("10.0.0.0/16", 8, 1)
+  availability_zone = format("%sa", var.region)
+
+  tags = {
+    Name = "management"
+  }
+}
+
+resource "aws_route_table_association" "route_table_public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.internet-gw.id
+}
+
+resource "aws_instance" "dev" {
+  ami               = data.aws_ami.ubuntu.id
+  availability_zone = "${var.region}a"
+  instance_type     = "${var.instance_type}"
+  subnet_id         = aws_subnet.public.id
+  associate_public_ip_address = true
+
+  user_data = data.coder_workspace.me.transition == "start" ? local.user_data_start : local.user_data_end
+  tags = {
+    Name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+    # Required if you are using our example policy, see template README
+    Coder_Provisioned = "true"
+  }
+}
